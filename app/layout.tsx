3515670f57a7c336d0001dd1b41c6e30d74b6db1
onderonur <onuronder.onr@gmail.com>
2022-11-21 23:21:48 +0300

    favicon tag is added

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#fff" />
       </head>
       <body>

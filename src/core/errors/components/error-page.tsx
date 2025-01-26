d22309c38e930cf554c51de91b08372a74e8cebd
onderonur <onuronder.onr@gmail.com>
2025-01-27 02:12:48 +0300

    minor style improvement for error page with long texts

--- src/core/errors/components/error-page.tsx
+++ src/core/errors/components/error-page.tsx
@@ -7,11 +7,11 @@ type ErrorPageProps = {
 export function ErrorPage({ statusCode, message, children }: ErrorPageProps) {
   return (
     <main className="mb-12 grid h-full place-content-center gap-4">
-      <h1 className="flex items-center">
-        <span className="text-foreground border-r-2 p-4 text-3xl font-bold">
+      <h1 className="flex min-w-0 flex-col items-center text-center md:flex-row md:text-left">
+        <span className="text-foreground border-b-2 p-4 text-3xl font-bold md:border-r-2 md:border-b-0">
           {statusCode}
         </span>
-        <span className="text-muted-foreground max-w-screen-sm p-4 text-lg font-semibold break-words">
+        <span className="text-muted-foreground w-full max-w-screen-sm p-4 text-lg font-semibold break-words">
           {message}
         </span>
       </h1>
